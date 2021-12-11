c69575a8f4d08b3a4dcb3705a35ac9eefe6ae44d
Lucas Mariano <lucasmgaldino@gmail.com>
2021-12-11 15:39:43 -0300

    Adicionando validações para os dados de entrada do endpoint.
    Caso a requisição HTTP não possua em seu corpo todos os dados obrigatórios e nos formatos exigidos, terá como resposta um código de erro 400 (bad request).

--- src/main/java/br/edu/ifrn/sga/contactservice/ContatoRequest.java
+++ src/main/java/br/edu/ifrn/sga/contactservice/ContatoRequest.java
@@ -3,7 +3,10 @@
  */
 package br.edu.ifrn.sga.contactservice;
 
-import lombok.AllArgsConstructor;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import lombok.Getter;
 import lombok.ToString;
 
@@ -13,12 +16,34 @@ import lombok.ToString;
  */
 @Getter
 @ToString
-@AllArgsConstructor
 public class ContatoRequest {
 
+	@NotBlank
 	private String nome;
+	@NotNull
+	@Email
 	private String email;
+	@NotBlank
 	private String assunto;
+	@NotBlank
 	private String mensagem;
+
+	/**
+	 * Contrutor.
+	 * 
+	 * @param umNome      um {@link String} que representa o nome do contatante.
+	 * @param umEmail     um {@link String} que representa o email do contatante.
+	 * @param umAssunto   um {@link String} que representa o assunto do contato.
+	 * @param umaMensagem um {@link String} que representa o conteúdo propriamente
+	 *                    dito da mensagem.
+	 */
+	public ContatoRequest(@NotBlank String umNome, @NotNull @Email String umEmail, @NotBlank String umAssunto,
+			@NotBlank String umaMensagem) {
+		super();
+		this.nome = umNome;
+		this.email = umEmail;
+		this.assunto = umAssunto;
+		this.mensagem = umaMensagem;
+	}
 
 }
--- src/main/java/br/edu/ifrn/sga/contactservice/controllers/ContatoController.java
+++ src/main/java/br/edu/ifrn/sga/contactservice/controllers/ContatoController.java
@@ -3,6 +3,8 @@
  */
 package br.edu.ifrn.sga.contactservice.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +19,9 @@ import br.edu.ifrn.sga.contactservice.ContatoRequest;
 public class ContatoController {
 
 	@PostMapping(value = "/api/contato")
-	public void entrarEmContato(@RequestBody ContatoRequest request) {
+	public void entrarEmContato(@RequestBody @Valid ContatoRequest request) {
 		System.out.println(request);
 	}
+	
 
 }
